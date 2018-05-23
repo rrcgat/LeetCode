74607cacfa27a9eec59cb4291dde782859827077
rrcgat <rrcgat@gmail.com>
2018-05-23 23:05:10 +0800

    Add brute-force solution

--- two-sum.lisp
+++ two-sum.lisp
@@ -5,3 +5,16 @@
            (if a
                (return (list a i))
                (setf (gethash (nth i nums) h) i))))))
+
+
+(defun two-sum-1 (nums target)
+  (let ((start 0)
+        (tmp))
+    (loop while (cdr nums) do
+         (setf tmp (cdr nums))
+         (loop for i from 1 below (length nums) do
+              (if (= (+ (car nums) (car tmp)) target)
+                  (return-from two-sum-1 (list start (+ start i)))
+                  (setf tmp (cdr tmp))))
+         (setf nums (cdr nums))
+         (incf start))))

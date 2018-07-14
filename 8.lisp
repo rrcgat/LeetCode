02b9a43af25d0a3f944a98a1f1ab68b35cedf5bd
rrcgat <rrcgat@gmail.com>
2018-07-14 15:56:04 +0800

    String to Integre(atoi)

---
+++ 8.lisp
@@ -0,0 +1,20 @@
+(defun my-atoi (str)
+  (let ((str (string-trim " " str))
+        (digit (make-array 12
+                           :fill-pointer 0
+                           :element-type 'character)))
+    (if (zerop (length str))
+        (return-from my-atoi 0)
+        (vector-push (elt str 0) digit))
+    (loop for s across (subseq str 1) do
+         (if (digit-char-p s)
+             (vector-push s digit)
+             (return)))
+    (handler-case (let ((res (parse-integer digit))
+                        (limit (expt 2 31)))
+                    (cond ((> res (1- limit))
+                           (1- limit))
+                          ((< res (- limit))
+                           (- limit))
+                          (t res)))
+      (error () 0))))

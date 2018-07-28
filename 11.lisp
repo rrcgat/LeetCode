d00206085a314925bf69bc181e8a17b336ed82c5
rrcgat <rrcgat@gmail.com>
2018-07-28 19:28:40 +0800

    Container With Most Water

---
+++ 11.lisp
@@ -0,0 +1,19 @@
+(defun max-area (height)
+  (let ((r 0)
+        (l (1- (length height)))
+        (area 0)
+        (max-area 0))
+    (loop while (< r l) do
+         (if (< (nth r height) (nth l height))
+             (shorten (nth r height) r 1)
+             (shorten (nth l height) l -1))
+         (if (> area max-area)
+             (setf max-area area)))
+    max-area))
+
+(defmacro shorten (short i step)
+  `(progn
+     (incf ,i ,step)
+     (setf area (* ,short (- l r)))
+     (loop while (and (< r l) (< (nth ,i height) ,short)) do
+          (incf ,i ,step))))
